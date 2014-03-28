75b419a73ea0e09be2c3f3dd15eed2b5d7cfaf48
Jérémy Barbet <jeremgraph@gmail.com>
2014-03-28 15:16:47 +0100

    Player design, basic controls

--- src/main.js
+++ src/main.js
@@ -21,7 +21,89 @@ analyser.on("isAd", function(isAd) {
   console.log("AD !!!");
 });
 
+audio.autoplay = true;
+audio.id = "audio-player";
+
 // audio.src = "http://listen.radionomy.com/fuzzy-and-groovy";
 audio.src = "http://sacem.iliaz.com/radionova.ogg";
-// audio.src = "http://sacem.iliaz.com/spotify.ogg";
-audio.play();
+audio.src = "http://sacem.iliaz.com/radionova.ogg";
+
+document.getElementById('player').appendChild(audio);
+
+var player = document.getElementById('audio-player');
+var btnPlay = document.getElementById('btn-play');
+var btnPause = document.getElementById('btn-pause');
+
+// Play button
+btnPlay.addEventListener('click', function() {
+  player.play();
+});
+
+// Pause button
+btnPause.addEventListener('click', function() {
+  player.pause();
+});
+
+document.getElementById('volume').addEventListener('change', function () {
+  // gainNode.gain.value = this.value;
+});
+
+player.addEventListener('timeupdate', function() {
+  document.getElementById('duration').innerHTML = parseInt(this.currentTime);
+});
+
+/*
+ * Save playlist url
+ */
+
+var tagsToReplace = {
+  '&': '&amp;',
+  '<': '&lt;',
+  '>': '&gt;',
+  '"': '&quot;',
+  "'": '&lsquo;'
+};
+
+function replaceTag(tag) {
+  return tagsToReplace[tag] || tag;
+}
+
+function safe_tags_replace(str) {
+  return str.replace(/[&<>"']/g, replaceTag);
+}
+
+document.getElementById('form-playlist').addEventListener('submit', function (evt) {
+  var urlValue = document.getElementById('input-url').value;
+  var urls = JSON.parse(localStorage.getItem('urls')) || [];
+  var safeUrl = safe_tags_replace(urlValue);
+
+  // Push to array
+  urls.push(safeUrl);
+
+  // Save
+  localStorage['urls'] = JSON.stringify(urls);
+
+  // Display
+  displayItem();
+
+  // Clean input
+  document.getElementById('input-url').value = '';
+  
+  // Avoid reload
+  evt.preventDefault();
+});
+
+// Read urls of localstorage
+function readUrls() {
+  var urls = JSON.parse(localStorage.getItem('urls')) || [];
+
+  return urls.map(function(url) {
+    return '<li>' + url + '</li>';
+  }).join('');
+}
+
+// Display items
+function displayItem() {
+  document.getElementById('playlist-items').innerHTML = readUrls();
+} displayItem();
+>>>>>>> Player design, basic controls
